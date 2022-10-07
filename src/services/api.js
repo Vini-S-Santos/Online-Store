3395f62e10e8ae3e3eae389284ea7ab44201cb12
Vinicius <visouza0@gmail.com>
2022-10-07 15:19:22 -0300

    Adiciona a 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,21 @@
 export async function getCategories() {
-  // Implemente aqui
+  try {
+    const categories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+    const response = await categories.json();
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  try {
+    const product = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+    const response = await product.json();
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function getProductById() {
