aedf476cddbdff8e15db9fce14a08f7b4261784d
Vinicius <visouza0@gmail.com>
2022-10-07 15:39:36 -0300

    adiciona 02

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </div>
+    );
+  }
+}
+
+export default Home;
