6563f6b5f7338464721df4f4084991d1ea18ee5a
Vinicius <visouza0@gmail.com>
2022-10-07 16:05:01 -0300

    adiciona a 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom/';
 import Home from './Pages/Home';
 import './App.css';
+import Cart from './Pages/Cart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/Cart" component={ Cart } />
       </Switch>
     </BrowserRouter>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
       <div data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
+        <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
+        <Link data-testid="shopping-cart-button" to="/Cart">Carrinho</Link>
       </div>
     );
   }
